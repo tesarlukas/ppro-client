6e31a5654f7e0c101981e5e7d8332e5c46e67b2b
tesarlukas <tesarlu2@uhk.cz>
2022-11-30 13:04:29 +0100

    Add prettier config so it's consistent across many workspaces

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ import TextField from './components/TextField';
 
 const App: React.FC = () => {
     return (
-        <div className='App font-bold text-green-200'>
-            <TextField text='Random text inside of textfield' />
+        <div className="App font-bold text-green-200">
+            <TextField text="Random text inside of textfield" />
         </div>
     );
 };
