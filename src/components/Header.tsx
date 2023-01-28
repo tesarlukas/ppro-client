991108eeb5d4d8a489cbc95e3c507b7ab1580560
tesarlukas <tesarlu2@uhk.cz>
2023-01-28 15:38:22 +0100

    Add conditional renderings for Header and implement logout feature

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,31 +3,42 @@ import { Link } from 'react-router-dom';
 import { UserContext } from '../context';
 
 export const Header: React.FC = () => {
-    const { user } = useContext(UserContext);
-    // const [user, setUser] = useState<string>(document.cookie.split('=')[0]);
-    // console.log(user);
+    const { user, setUser } = useContext(UserContext);
 
-    // useEffect(() => {
-    //     setUser(document.cookie.split('=')[0]);
-    //     console.log('ahoj');
-    // }, [user]);
+    const logout = (): void => {
+        document.cookie = `${user}= ; expires = Thu, 01 Jan 1970 00:00:00 GMT`;
+        setUser('');
+    };
 
     return (
         <header className="flex bg-white w-screen h-24 justify-between items-center p-4 shadow-md">
             <Link to="/">
                 <h1 className="text-black">Movie Database</h1>
             </Link>
-            <nav>
+            <nav className="flex">
                 <Link className="text-black p-2 text-lg" to="/filmworks">
                     Listing
                 </Link>
-                <Link className="text-black p-2 text-lg" to="/login">
-                    Login
-                    {user}
-                </Link>
-                <Link className="text-black p-2 text-lg" to="/register">
-                    Register
-                </Link>
+                {user === '' ? (
+                    <>
+                        <Link className="text-black p-2 text-lg" to="/login">
+                            Login
+                        </Link>
+                        <Link className="text-black p-2 text-lg" to="/register">
+                            Register
+                        </Link>
+                    </>
+                ) : (
+                    <>
+                        <div className="text-black p-2 text-lg">
+                            Logged as{' '}
+                            <Link to={'/'} className="text-blue-600">
+                                {user}
+                            </Link>
+                        </div>
+                        <button onClick={logout}>Logout</button>
+                    </>
+                )}
             </nav>
         </header>
     );
